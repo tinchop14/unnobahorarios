324e65a38cf92da4301bfcb298d1b5c01d34359c
martin <tinchop.14@gmail.com>
2020-03-27 16:30:11 -0300

    primer a github

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    public mobileFriendlyZoomSetting = '150%';
+  constructor(public actionSheetController: ActionSheetController) {}
 
-  constructor() {}
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Elija el día',
+      buttons: [{
+        text: 'Hoy',
+        role: 'destructive',
+        icon: 'today-outline',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Mañana',
+        icon: 'calendar-outline',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 
-}
+  }
